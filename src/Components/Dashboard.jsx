08396a1adadd70a827e6d994837dbc5fee07ded8
strong <strong.business.info@gmail.com>
2023-11-06 14:29:22 +0400

    Updated Document

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,24 +1,28 @@
 import { useDisclosure } from '@mantine/hooks';
 import { AppShell } from '@mantine/core';
 import Header from './Header';
-
+import ThemeSwitcher from './ThemeSwitcher'
 
 export function Dashboard() {
 	const [opened, { toggle }] = useDisclosure();
-	
 
-  return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-      padding="md"
-    >
 
-      <Header toggle={toggle} opened={opened}/>
-		
-      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+	return (
+		<AppShell
+			header={{ height: 60 }}
+			navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+			padding="md"
+		>
 
-			<AppShell.Main>Main</AppShell.Main>
+			<Header toggle={toggle} opened={opened} />
+
+			<AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+			<AppShell.Main>
+				Main
+				<ThemeSwitcher />
+			</AppShell.Main>
+			
 		</AppShell>
 	);
 }
