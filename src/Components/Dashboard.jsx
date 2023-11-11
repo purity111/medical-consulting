0c4ed07186de9263b76febe1c6a85b7bf0d95b41
strong <strong.business.info@gmail.com>
2023-11-12 02:03:12 +0400

    add routing to navbar

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -2,26 +2,44 @@ import { useDisclosure } from '@mantine/hooks';
 import { AppShell } from '@mantine/core';
 import Header from './Header';
 import Navbar from './Navbar';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import ConsultationsLog from './ConsultationsLog';
+import Overview from './Overview';
+import Appointments from './Appointments';
+import Patients from './Patients';
+import Messages from './Messages';
+import Setting from './Setting';
 
 function Dashboard() {
 	const [opened, { toggle }] = useDisclosure();
 
 
 	return (
-		<AppShell
-			header={{ height: 60 }}
-			navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-			padding="md"
-		>
+		<BrowserRouter>
+			<AppShell
+				header={{ height: 60 }}
+				navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+				padding="md"
+			>
 
-			<Header toggle={toggle} opened={opened} />
+				<Header toggle={toggle} opened={opened} />
 
-			<Navbar/>
-			<AppShell.Main>
-				Main
-			</AppShell.Main>
+				<Navbar/>
 
-		</AppShell>
+				<AppShell.Main>
+					<Routes>
+						<Route path="/Overview" element={<Overview />} />
+						<Route path="/Appointments" element={<Appointments />} />
+						<Route path="/Patients" element={<Patients />} />
+						<Route path="/Consultationslog" element={<ConsultationsLog />} />
+						<Route path="/Messages" element={<Messages />} />
+						<Route path="/Setting" element={<Setting />} />
+						{/* <Route path="/Logout" element={<ConsultationsLog />} /> */}
+      				</Routes>
+				</AppShell.Main>
+
+			</AppShell>
+		</BrowserRouter>
 	);
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,46 +1,48 @@
 import { useState } from 'react';
 import { IconHome2, IconCalendarEvent, IconUser, IconArticle, IconMessage2, IconSettings, IconLogout2, IconPlus} from '@tabler/icons-react';
 import { Box, NavLink, Text, Anchor, AppShell, Group, Button} from '@mantine/core';
+import { Link } from 'react-router-dom';
+
 
 const data = [
   {
-    label: 'Overview',
+    label: <Text component={Link} variant='link' to='/Overview'>Overview</Text>,
     leftSection: <IconHome2 size="1rem" stroke={1.5} />,
     mt: 35,
     color: "blue"
   },
   {
     headingText: <Text c="gray" size="xs" mt={20}>PATIENT RELATION</Text>,
-    label: 'Appointments',
+    label: <Text component={Link} variant='link' to='/Appointments'>Appointments</Text>,
     leftSection: <IconCalendarEvent size="1rem" stroke={1.5} />,
     mt: 11,
     color: "blue"
   },
   {
-    label: 'Patients',
+    label: <Text component={Link} variant='link' to='/Patients'>Patients</Text>,
     leftSection: <IconUser size="1rem" stroke={1.5} />,
     color: "blue"
   },
   {
-    label: 'Consultations Log',
+    label: <Text component={Link} variant='link' to='/Consultationslog'>Consultations Log</Text>,
     leftSection: <IconArticle size="1rem" stroke={1.5} />,
     color: "blue"
   },
   {
     headingText: <Text c="gray" size="xs" mt={20}>SUPPORT</Text>,
-    label: 'Messages',
+    label: <Text component={Link} variant='link' to='/Messages'>Messages</Text>,
     leftSection: <IconMessage2 size="1rem" stroke={1.5} />,
     mt: 11,
     color: "blue"
   },
   {
-    label: 'Settings',
+    label: <Text component={Link} variant='link' to='/Setting'>Setting</Text>,
     leftSection: <IconSettings size="1rem" stroke={1.5} />,
     mt: 400,
     color: "blue"
   },
    {
-    label: 'Logout',
+    label: <Text component={Link} variant='link' to='/Logout'>Logout</Text>,
     leftSection: <IconLogout2 size="1rem" stroke={1.5} />,
     color: "red"
   },
