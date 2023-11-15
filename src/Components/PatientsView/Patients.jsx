c80ed1926df0e352c1622b64ef7ff7ee672fc693
strong <strong.business.info@gmail.com>
2023-11-15 20:01:20 +0400

    Added Searchbar to the PatientsView

--- src/Components/PatientsView/Patients.jsx
+++ src/Components/PatientsView/Patients.jsx
@@ -1,9 +1,24 @@
 import PatientsGallery from './PatientsGallery';
+import { useState } from "react";
+import { Flex } from "@mantine/core";
+import SearchBarFilter from "../SearchBarFilter";
 
 function Patients() {
-  return (
-	<PatientsGallery />
-  );
+	const [search, setSearch] = useState("");
+
+	return (
+		<>
+			<Flex mih={50} gap="20" direction="column" wrap="wrap">
+				<SearchBarFilter
+					filterTable={(name) => setSearch(name)}
+					placeholder="Search for patient"
+					width={300}
+				/>
+
+				<PatientsGallery searchWord={search} />
+			</Flex>
+		</>
+	);
 }
 
 export default Patients;
