7b387969a21af91819babe7954eed1a1831dcaec
jun <tanabejun90@gmail.com>
2023-11-15 15:31:42 +0400

    settings

--- src/Components/Setting.jsx
+++ src/Components/Setting.jsx
@@ -1,6 +1,19 @@
-function Setting (){
+import { Title, Text, Grid, Button, Group } from '@mantine/core';
+function Setting() {
     return (
-        <div>Setting</div>
+        <><Group justify="space-between">
+            <Group>
+                <><Title size="h1" ml={20}>
+                    Settings
+                </Title>
+                </>
+            </Group>
+            <Group>
+                <Button variant="default" size="md">Cancel</Button>
+                <Button variant="filled" size="md">Save</Button>
+            </Group>
+        </Group>
+        <Group><Text size="md" ml={20}>Manage Your Account</Text></Group></>
     );
 
 }
