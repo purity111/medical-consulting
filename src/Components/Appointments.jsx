2cfec7688a4adc9d4ab02f11868c281ece1db436
jun <tanabejun90@gmail.com>
2023-11-15 18:26:50 +0400

    added appointment sections

--- src/Components/Appointments.jsx
+++ src/Components/Appointments.jsx
@@ -1,6 +1,48 @@
+import { Grid, Card, Text, Title, TextInput, Checkbox } from '@mantine/core';
+import { Calendar } from '@mantine/dates';
+
 function Appointments() {
 	return (
-		<div>Appointments</div>
+		<Grid>
+			<Grid.Col span={8}>
+				<Card shadow="sm" padding="lg" radius="md" withBorder h={1000}>
+
+				</Card>
+			</Grid.Col>
+			<Grid.Col span={4}>
+				<Grid>
+					<Grid.Col>
+						<Card shadow="sm" padding="lg" radius="md" withBorder>
+							<Calendar size='xl' />
+						</Card>
+					</Grid.Col>
+					<Grid.Col>
+						<Card shadow="sm" padding="lg" radius="md" withBorder>
+							<Title order={5} ta="center">Tasks</Title>
+							<TextInput mt={10}
+								placeholder="Type a task"
+							/>
+							<Checkbox
+								mt={20}
+								label="This is a task"
+							/>
+							<Checkbox
+								mt={20}
+								label="This is a task"
+							/>
+							<Checkbox
+								mt={20}
+								label="This is a task"
+							/>
+							<Checkbox
+								mt={20}
+								label="This is a task"
+							/>
+						</Card>
+					</Grid.Col>
+				</Grid>
+			</Grid.Col>
+		</Grid>
 	);
 
 }
