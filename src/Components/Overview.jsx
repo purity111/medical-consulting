23ba58b9bc67948845e911735307fc02a528de7b
strong <strong.business.info@gmail.com>
2023-11-17 15:31:24 +0400

    adding card to consultation log wedget

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -1,4 +1,4 @@
-import { Flex, Grid, Skeleton } from "@mantine/core";
+import { Flex, Grid, Skeleton, Card, Group } from "@mantine/core";
 import MainHeader from "./MainHeader";
 import ConsulatationLogWidget from "./ConsultationsView/ConsultationLogWidget";
 const child = <Skeleton height={140} radius="md" animate={false} />;
@@ -19,11 +19,11 @@ function Overview() {
       <Flex mih={50} gap="20" direction="column" wrap="wrap">
         <Grid>
           <Grid.Col span={{ base: 12, xs: 8 }}>
-            <ConsulatationLogWidget />
+            <Card shadow="sm" padding="lg" radius="md">
+              <ConsulatationLogWidget />
+            </Card>
           </Grid.Col>
-          <Grid.Col span={{ base: 12, xs: 4 }}>
-            <ConsulatationLogWidget />
-          </Grid.Col>
+          <Grid.Col span={{ base: 12, xs: 4 }}></Grid.Col>
           <Grid.Col span={{ base: 12, xs: 12 }}>{child}</Grid.Col>
           <Grid.Col span={{ base: 12, xs: 12 }}>{child}</Grid.Col>
         </Grid>
