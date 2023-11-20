c1b41dc21c23dc755ea4674f16b5133708094e94
strong <strong.business.info@gmail.com>
2023-11-20 09:51:47 +0400

    addes Patient widget

---
+++ src/Components/PatientsView/PatientWidget.jsx
@@ -0,0 +1,11 @@
+import PatientsGallery from "../PatientsView/PatientsGallery";
+import { Flex } from "@mantine/core";
+import { useState } from "react";
+import MainHeader from "../MainHeader";
+import SearchBarFilter from "../SearchBarFilter";
+
+function Patients(props) {
+  return <></>;
+}
+
+export default Patients;
