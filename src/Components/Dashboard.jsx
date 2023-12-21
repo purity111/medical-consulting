a8efc2779bcf8be88ac9ac1fe05faf06b1032d4c
strong <strong.business.info@gmail.com>
2023-12-22 02:01:44 +0400

    Fix Double imports

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -2,7 +2,16 @@ import { useDisclosure } from "@mantine/hooks";
 import { AppShell } from "@mantine/core";
 import Header from "./Header";
 import Navbar from "./Navbar";
-import {BrowserRouter } from "react-router-dom";
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ConsultationsLog from "./ConsultationsView/ConsultationsLog";
+import Overview from "./Overview/Overview";
+import Appointments from "./AppointmentsView/Appointments";
+import Patients from "../Components/PatientsView/Patients";
+import Messages from "./MessagesView/Messages";
+import Setting from "./Settings/Setting";
+import PatientProfile from "./PatientsView/PatientProfile";
+import NewConsultation from "./PatientsView/NewConsultation";
 import Routing from "./Routing";
 
 function Dashboard(props) {
