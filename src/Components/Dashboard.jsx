96e42495450db912ded07644f65397bcd0327b9e
strong <strong.business.info@gmail.com>
2023-12-23 09:47:36 +0000

    delete unnecessary imports

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -4,14 +4,6 @@ import Header from "./Header";
 import Navbar from "./Navbar";
 
 import {BrowserRouter } from "react-router-dom";
-// import ConsultationsLog from "./ConsultationsView/ConsultationsLog";
-// import Overview from "./Overview/Overview";
-// import Appointments from "./AppointmentsView/Appointments";
-// import Patients from "../Components/PatientsView/Patients";
-// import Messages from "./MessagesView/Messages";
-// import Setting from "./Settings/Setting";
-// import PatientProfile from "./PatientsView/PatientProfile";
-// import NewConsultation from "./PatientsView/NewConsultation";
 import Routing from "./Routing";
 
 function Dashboard(props) {
