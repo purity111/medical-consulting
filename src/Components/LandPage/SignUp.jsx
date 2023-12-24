1be5f15fbe1993f8d5393708d6cf92d570e5c14c
ekuinox <depkey@me.com>
2023-12-24 22:05:17 +0400

    Signup fixed

--- src/Components/LandPage/SignUp.jsx
+++ src/Components/LandPage/SignUp.jsx
@@ -1,52 +1,141 @@
 import { useState } from 'react';
-import { Stepper, Button, Group, Grid, Center, Stack, Title } from '@mantine/core';
-import SignUpForm from './SignUpForm';
+import { Stepper, Button, Group, Grid, Center, Stack, Title, PasswordInput, Input, Space } from '@mantine/core';
+import { IMaskInput } from 'react-imask';
+import { useDisclosure } from '@mantine/hooks';
+import { IconChevronDown } from '@tabler/icons-react';
+import { DateInput } from '@mantine/dates';
 
 function SignUp() {
     const [active, setActive] = useState(0);
     const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
     const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+    const [visible, { toggle }] = useDisclosure(false);
+    const [value, setValue] = useState(null);
 
     return (
         <>
-            <Grid mt={100}>
+            <Grid mt={150}>
                 <Grid.Col span={3}></Grid.Col>
                 <Grid.Col span={6}>
                     <Stepper active={active}>
                         <Stepper.Step label="First step" description="Patient Information">
-                            <Stack>
-                                <SignUpForm label1="First Name" placeholder1="First Name..."
-                                    label2="Last Name" placeholder2="Last Name..."
-                                />
-                                <SignUpForm label1="Email" placeholder1="Email..."
-                                    label2="Emirates ID" placeholder2="Emirates ID..."
-                                />
-                                <SignUpForm label1="Contacr Number" placeholder1="Contacr Number..."
-                                    label2="Emergency Contact" placeholder2="Emergency Contact..."
-                                />
-                                <SignUpForm label1="Date of birth" placeholder1="Date of birth..."
-                                    label2="Gender" placeholder2="Gender..."
-                                />
-                            </Stack>
+                            <Grid>
+                                <Grid.Col span={3}></Grid.Col>
+                                <Grid.Col span={3}>
+                                    <Input.Wrapper label="First Name" withAsterisk >
+                                        <Input size="md" placeholder="First Name..." />
+                                    </Input.Wrapper>
+                                    <Space h="md" />
+                                    <Input.Wrapper label="Email" withAsterisk >
+                                        <Input size="md" placeholder="Email..." />
+                                    </Input.Wrapper>
+                                    <Space h="md" />
+                                    <Input.Wrapper label="Contact Number" withAsterisk >
+                                        <Input component={IMaskInput} size="md" mask="+971 (000) 0000-000" placeholder="Contact Number..." />
+                                    </Input.Wrapper>
+                                    <Space h="md" />
+                                    <Input.Wrapper label="Date Of Birth" withAsterisk >
+                                        <DateInput
+                                            value={value}
+                                            onChange={setValue}
+                                            placeholder="Date of birth..."
+                                            size='md'
+                                        />
+                                    </Input.Wrapper>
+                                </Grid.Col>
+                                <Grid.Col span={3}>
+                                    <Input.Wrapper label="Last Name" withAsterisk >
+                                        <Input size="md" placeholder="Last Name..." />
+                                    </Input.Wrapper>
+                                    <Space h="md" />
+                                    <Input.Wrapper label="Emirates ID" withAsterisk >
+                                        <Input component={IMaskInput} size="md" mask="000-000-0000000-0" placeholder="Emirates ID..." />
+                                    </Input.Wrapper>
+                                    <Space h="md" />
+                                    <Input.Wrapper label="Emergency Contact" >
+                                        <Input component={IMaskInput} size="md" mask="+971 (000) 0000-000" placeholder="Emergency Contact..." />
+                                    </Input.Wrapper>
+                                    <Space h="md" />
+                                    <Input.Wrapper label="Gender" withAsterisk >
+                                        <Input
+                                            component="select"
+                                            rightSection={<IconChevronDown size={14} stroke={1.5} />}
+                                            size='md'
+
+                                        >
+                                            <option value="1">None</option>
+                                            <option value="2">Male</option>
+                                            <option value="3">Female</option>
+                                        </Input>
+                                    </Input.Wrapper>
+                                </Grid.Col>
+                            </Grid>
                         </Stepper.Step>
                         <Stepper.Step label="Second step" description="Create Password">
-                            <SignUpForm label1="Password" placeholder1="Password"
-                                label2="Confirm Password" placeholder2="Confirm Password..."
-                            />
+                            <Grid>
+                                <Grid.Col span={3}></Grid.Col>
+                                <Grid.Col span={3}>
+                                    <PasswordInput
+                                        label="Password"
+                                        placeholder="Password.."
+                                        visible={visible}
+                                        onVisibilityChange={toggle}
+                                        withAsterisk
+                                    />
+                                </Grid.Col>
+                                <Grid.Col span={3}>
+                                    <PasswordInput
+                                        label="Confirm password"
+                                        placeholder="Password.."
+                                        visible={visible}
+                                        onVisibilityChange={toggle}
+                                        withAsterisk
+                                    />
+                                </Grid.Col>
+                            </Grid>
                         </Stepper.Step>
                         <Stepper.Step label="Final step" description="Health Information">
-                            <SignUpForm label1="Insurance type:" placeholder1="Insurance type.."
-                                label2="Blood Type:" placeholder2="Blood type.."
-                            />
-                            <SignUpForm label1="Allergies:" placeholder1="Allergies.."
-                                label2="Additional Note:" placeholder2="Additional Note:..."
-                            />
+                            <Grid>
+                                <Grid.Col span={3}></Grid.Col>
+                                <Grid.Col span={3}>
+                                    <Input.Wrapper label="Insurance Type" withAsterisk >
+                                        <Input size="md" placeholder="Insurance Type..." />
+                                    </Input.Wrapper>
+                                    <Space h="md" />
+                                    <Input.Wrapper label="Allergies" withAsterisk >
+                                        <Input size="md" placeholder="Allergies..." />
+                                    </Input.Wrapper>
+                                </Grid.Col>
+                                <Grid.Col span={3}>
+                                    <Input.Wrapper label="Blood Type" withAsterisk >
+                                        <Input
+                                            component="select"
+                                            rightSection={<IconChevronDown size={14} stroke={1.5} />}
+                                            size='md'
+
+                                        >
+                                            <option value="1">None</option>
+                                            <option value="2">A-</option>
+                                            <option value="3">A+</option>
+                                            <option value="4">B-</option>
+                                            <option value="5">B+</option>
+                                            <option value="6">O-</option>
+                                            <option value="7">O+</option>
+                                            <option value="8">AB-</option>
+                                            <option value="9">AB+</option>
+                                        </Input>
+                                    </Input.Wrapper>
+                                    <Space h="md" />
+                                    <Input.Wrapper label="Additional Note">
+                                        <Input size="md" placeholder="Additional Note..." />
+                                    </Input.Wrapper>
+                                </Grid.Col>
+                            </Grid>
                         </Stepper.Step>
                         <Stepper.Completed>
                             <Center>
                                 <Title order={2}>Completed, account has been created successfully!</Title>
                             </Center>
-
                         </Stepper.Completed>
                     </Stepper>
 
