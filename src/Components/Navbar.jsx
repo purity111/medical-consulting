472bd07f094de41f75c881b66c39d73aad1859ee
strong <strong.business.info@gmail.com>
2023-12-26 01:14:01 +0400

    Fixed navbar height on smaller screens

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -157,7 +157,7 @@ function Navbar(props) {
       key: 6,
       label: "Setting",
       leftSection: <IconSettings size="1rem" stroke={1.5} />,
-      mt: isLongDisplay ? '8vh' : '52vh',
+      mt: isLongDisplay ? '20vh' : '50vh',
       color: "blue",
       path: "/adminDashboard/Setting",
     },
