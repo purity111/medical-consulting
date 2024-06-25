6092945ea7e9ba2198308dbf045ff057aaf79507
strong <strong.business.info@gmail.com>
2024-06-25 20:02:38 +0400

    fix small issue with mfa

--- src/Components/LandPage/Login.jsx
+++ src/Components/LandPage/Login.jsx
@@ -33,9 +33,9 @@ function Login() {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate("/doctorDashboard/overview/");
+      // navigate("/doctorDashboard/overview/");
       Cookies.set("email", email);
-      // setMfa(false);
+      setMfa(false);
     } catch (err) {
       console.error(err);
     }
@@ -54,36 +54,40 @@ function Login() {
         </Group>
       </AppShell.Header>
 
-      <Grid mt={200}>
-        <Grid.Col span={isMobile ? 1 : 4.5}></Grid.Col>
-        <Grid.Col span={isMobile ? 10 : 3}>
-          <Card shadow="sm" withBorder radius="md">
-            <Title order={2}>Login</Title>
-            <Space h="xl" />
-            <Text c="red">{error ? "Invalid email or password" : null}</Text>
-            <Input.Wrapper label="Email" withAsterisk>
-              <Input
-                size="lg"
-                placeholder="Email..."
-                onChange={(event) => setEmail(event.currentTarget.value)}
-              />
-            </Input.Wrapper>
-            <Space h="lg" />
-            <Input.Wrapper label="Password" withAsterisk>
-              <PasswordInput
-                size="lg"
-                placeholder="Password..."
-                onChange={(event) => setPassword(event.currentTarget.value)}
-              />
-            </Input.Wrapper>
-            <Space h="xl" />
-            <Group gap="lg" justify="space-between">
-              <Button onClick={signIn}>Login</Button>
-              <Button variant="subtle">FORGET PASSWORD?</Button>
-            </Group>
-          </Card>
-        </Grid.Col>
-      </Grid>
+      {mfa ? (
+        <Grid mt={200}>
+          <Grid.Col span={isMobile ? 1 : 4.5}></Grid.Col>
+          <Grid.Col span={isMobile ? 10 : 3}>
+            <Card shadow="sm" withBorder radius="md">
+              <Title order={2}>Login</Title>
+              <Space h="xl" />
+              <Text c="red">{error ? "Invalid email or password" : null}</Text>
+              <Input.Wrapper label="Email" withAsterisk>
+                <Input
+                  size="lg"
+                  placeholder="Email..."
+                  onChange={(event) => setEmail(event.currentTarget.value)}
+                />
+              </Input.Wrapper>
+              <Space h="lg" />
+              <Input.Wrapper label="Password" withAsterisk>
+                <PasswordInput
+                  size="lg"
+                  placeholder="Password..."
+                  onChange={(event) => setPassword(event.currentTarget.value)}
+                />
+              </Input.Wrapper>
+              <Space h="xl" />
+              <Group gap="lg" justify="space-between">
+                <Button onClick={signIn}>Login</Button>
+                <Button variant="subtle">FORGET PASSWORD?</Button>
+              </Group>
+            </Card>
+          </Grid.Col>
+        </Grid>
+      ) : (
+        <Mfa />
+      )}
     </AppShell>
   );
 }
